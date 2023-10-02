a49d12399dc24ad7440a1dfa3be5be03e099a970
Rasool Personal <rasoolpersonal@Rasools-MacBook-Pro-2.local>
2023-10-02 22:58:46 +0530

    complete cli using nest-commander

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CowSayModule } from './cow-say/cow-say.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CowSayModule],
 })
 export class AppModule {}
